37ec42d7cf746143dd2be7248d79c689facf5aad
Dave <larapenta@hotmail.com>
2016-07-24 19:10:53 +1000

    Update Power_Meter_Mk2_12E-1v1.ino

--- Power_Meter_Mk2_12E-1v1.ino
+++ Power_Meter_Mk2_12E-1v1.ino
@@ -45,10 +45,8 @@ float eltimeend;
 <<<<<<< HEAD:Power_Meter_Mk2_12E.ino
 const char* ssid = "Band-NG-N3";    // set your local network SSID & password
 const char* password = "amersham";  
-=======
 const char* ssid = "your SSID";    // set your local network SSID
 const char* password = "your Password";  
->>>>>>> origin/master:Power_Meter_Mk1_12E_RCS_Ver.ino
 const unsigned int meter_pulses = 1000; // set for your meter eg 1000/KW, 800/Kw for Sprint meters
  
 // Create an instance of the server and specify the port to listen on
