ac9f9e885ba6f41a35446744ca6351fe4be930bd
Dave <larapenta@hotmail.com>
2016-07-24 18:54:45 +1000

    Revised Power Meter
    Correction to missing IR Module info and removal of weather station and
    wireless functions

--- Power_Meter_Mk2_12E.ino
+++ Power_Meter_Mk2_12E.ino
@@ -6,46 +6,32 @@
  *  a digital electric meter and converts them into kilowatt power consumption.
  *  Two light modules are required, a standard light reader module and 
  *  an Infra Red reader module (on some meters).
- *  A standard wireless transmitter AND receiver and RC power sockets  are
+ *  A standard 433Mhz wireless transmitter AND receiver and RC power sockets  are
  *  required. The receiver is used to intially discover the codes the remote 
  *  control unit is using so they can be incorporated into the software.
  *  The weather station is optional and measures temperature, barometric pressure
- *  and humidity. The data may then be retrieved via wifi by PC, tablet, smartphone or micro 
- *  as a standard web page requests. RC switching is done in the same way. 
- *  The form of power consumption or weather requests is -
- *     http://local_server_ip_address/gpio/0 to 9
+ *  and humidity. The Wireless relay point is optional, this would permit switching of 
+ *  433Mhz (or 315Mhz) wireless sockets via WiFi command.
+ *  Data for the above power and weather information may be retrieved via wifi 
+ *  by PC, tablet, smartphone or micro Pi as a standard web page requests. 
+ *  The form of power consumption requests is -
+ *     http://local_server_ip_address/gpio/0 to 5
  *  A typical request from a Pi running PHP to request current power usage would be :
  *  $json_string = file_get_contents(http://192.168.0.100/gpio/4);  which would return the power in watts
  *  Wireless RC requests/commands are e.g. -   
- *      http://local_ip_address/sw1/0 or 1 0=OFF, 1=ON (or sw2, sw3,... etc)
- *  example - http://192.168.0.100/sw3/1
- *  The server_ip is the local IP address of the ESP8266-12e module, 
- *  and will be printed to Serial when the module is connected
- *  e.g. 192.168.0.100 required more if using DHP
+ *  The server_ip is the local IP address of the ESP8266-12e module. 
+ *
  *  Power meter software Dave Clapham - infinityab56@gmail.com
  */
-
+ 
 #include <ESP8266WiFi.h>
-#include <RCSwitch.h>
-#include <Adafruit_BMP085.h>
-#include <UtilityFunctions.h>
-#include <Wire.h>
-#include <DHT.h>
-
-#define I2C_SCL 1   // pin SD2 12      // Barometric Pressure Sensor (BMP085)
-#define I2C_SDA 3  // pin SD3 - 13      // Barometric Pressure Sensor (BMP085)
-#define DHTPIN 14       // pin D5 Temp/Humidity GPIO pin (DHT11)
-#define DHTTYPE DHT11   // DHT 11 
 
 //globals
-int lc=0;
-bool complete=false;
-bool bmp085_present=true;
-float bt,bp,ba,dhtt,dhth;
-char btmp[20],bprs[20],balt[20],tin[20],tout[20],tatt[20],dhhumi[20],dhtemp[20]; 
-uint32_t state=0;
-char szT[30];
 
+const int aggmax = 3;  // // the number of aggreagate samples
+const int $debounce = 150;  // debounce in ms
+float aggpower[aggmax], aggtotal;  // = {  0, 0, 0, 0, 0, 0,};       // an array of power calculations
+int aggposition;
 boolean debug = false;  // turn serial port debug OFF/ON
 boolean pulsehigh = true;
 boolean irpulsehigh = false;
@@ -54,39 +40,29 @@ const String st_html = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCT
 float power;
 float eltimest;
 float eltimeend;
-RCSwitch mySwitch = RCSwitch();
 
 // ******** USER ENTRIES *************
-const char* ssid = "Band-NG-N3";    // set your local network SSID
+const char* ssid = "Band-NG-N3";    // set your local network SSID & password
 const char* password = "amersham";  
 const unsigned int meter_pulses = 1000; // set for your meter eg 1000/KW, 800/Kw for Sprint meters
-
-Adafruit_BMP085 bmp;
-// OneWire  ds(DS18B20);  // on pin 5 for ds18b20
-DHT dht(DHTPIN, DHTTYPE, 15);
+ 
 // Create an instance of the server and specify the port to listen on
 
 WiFiServer server(80);
 
 void setup() {
+  aggposition = 0;
+  for (int sample = 0; sample < aggmax; sample++) {
+  aggpower[sample] = 0;   // initialise aggregate power
+  }
   if (!debug) pinMode(2, OUTPUT);
   pinMode(4, INPUT); // prepare GPIO ports, set up pulse mode input (D2)
   pinMode(5, OUTPUT); // External Meter Pulse LED (D1)
   pinMode(15, OUTPUT); // External IR Pulse LED (D8)
-  pinMode(16, OUTPUT);  // RC transmitter Din, *via resistors, TX could be < 12v (D0)
   pinMode(12, INPUT);  // Infra Red Test Light Detector (grid power use) (D6)
-  mySwitch.enableTransmit(16);  // RC TX module is connected to ESP8266 PIN #16
-  mySwitch.setPulseLength(250);  // Optional set pulse length for RC TX.
-  mySwitch.setRepeatTransmit(3);  // Optional set number of transmission repetitions for RC TX.
-  // mySwitch.setProtocol(2);  // Optional set protocol (default is 1, will work for most outlets)
-   
-  Wire.begin(I2C_SDA, I2C_SCL); // Setup BMP085 (i2c)
-    if (!bmp.begin()) {
-    Serial.println("No BMP085 sensor detected!");
-    bmp085_present=false;
-  }
+
   delay(3000);
-  if (debug) Serial.begin(115200);
+  if (debug) Serial.begin(9600);
   delay(500);
   if (!debug) digitalWrite(2,0);  // use TX LED as indicator
     else digitalWrite(15, 0); // switch on external LED
@@ -117,9 +93,7 @@ void setup() {
 }
 
 void loop() {
- // unsigned int ar = analogRead(A0);  // read analog input for power etc
- // delay(500);
- // Serial.println("Analogue Read Value : " + String(ar));
+
   if (digitalRead(12) == LOW && irpulsehigh) {   // looking for change IR LED
     irpulsehigh = false;
     gridpower = 5;  // need to make sure definately NOT on grid power
@@ -128,7 +102,7 @@ void loop() {
   } else {
     if (digitalRead(12) == HIGH && !irpulsehigh) {   // looking for change IR LED
       irpulsehigh = true;
-      gridpower = 5;
+      gridpower = 5;   // set grid power detector
       if (debug) digitalWrite(15,1);  // LED indicator on this pin (D8)
          else digitalWrite(2,1);  // use TX LED as indicator
     }
@@ -137,20 +111,38 @@ void loop() {
     pulsehigh = false;
     eltimeend = millis();
     gridpower--;
-    if (gridpower > 0) {
-      power = (float) -1 *3600 * 1000 / (meter_pulses * (eltimeend - eltimest)); 
-      } else {
-        power = (float) 3600 * 1000 / (meter_pulses * (eltimeend - eltimest)); //grid power being drawn
+    if (gridpower > 0) {      // calculate current power
+      power = (float)-1 * 3600 * 1000 / (meter_pulses * (eltimeend - eltimest)); 
+      if (aggposition >= aggmax) {
+        aggposition = 0;
+        aggpower[aggposition] = (float)power;   // store power drawn from grid in circular buffer
+        aggposition++;
+        }
+      else {
+        aggpower[aggposition] = (float)power;
+        aggposition++;
+        }
+      } else {   //  on solar power
+        power = (float) 3600 * 1000 / (meter_pulses * (eltimeend - eltimest)); // solar power being drawn
         gridpower = 0;  // ensure it stays at zero
+        if (aggposition >= aggmax) {
+          aggposition = 0;
+          aggpower[aggposition] = (float)power;    // storing power drawn from solar in circular buffer
+          aggposition++;
+          }
+        else {
+        aggpower[aggposition] = (float)power;
+        aggposition++;
+        }
       }    
     eltimest = eltimeend;
     digitalWrite(5,0);  // LED indicator on this pin (D1)
-    delay(50);  // debounce 100ms
+    delay($debounce);  // debounce
     } else {
     if (digitalRead(4) == HIGH && !pulsehigh) {
       pulsehigh = true;
       digitalWrite(5,1);  // LED indicator on this pin (D1)
-      delay(50);  // debounce 100ms        
+      delay($debounce);  // debounce        
     } 
  }
 
@@ -172,11 +164,11 @@ void loop() {
       gridpower = 5;
     }
   }
-  delay(50);
+  delay(30);
   timer--;
   if (timer <= 0) {
-    Serial.println("No Request");
-    client.print(st_html + "No Request" + "</html>\n");
+    Serial.println("Bad Request");
+    client.print(st_html + "Bad Request" + "</html>\n");
     client.flush();
     client.stop();
     return;  
@@ -200,28 +192,8 @@ void loop() {
     message = 3;  
   else if (req.indexOf("/gpio/4") != -1)
     message = 4; 
-   else if (req.indexOf("/gpio/5") != -1)
+  else if (req.indexOf("/gpio/5") != -1)
     message = 5;   
-  else if (req.indexOf("/gpio/6") != -1)
-    message = 6;
-  else if (req.indexOf("/gpio/7") != -1)
-    message = 7;  
-  else if (req.indexOf("/gpio/8") != -1)
-    message = 8; 
-  else if (req.indexOf("/gpio/9") != -1)
-    message = 9;   
-  else if (req.indexOf("/sw1/0") != -1)
-    message = 10;
-  else if (req.indexOf("/sw1/1") != -1)
-    message = 11;
-  else if (req.indexOf("/sw2/0") != -1)
-    message = 12;
-  else if (req.indexOf("/sw2/1") != -1)
-    message = 13;
-  else if (req.indexOf("/sw3/0") != -1)
-    message = 14;
-  else if (req.indexOf("/sw3/1") != -1)
-    message = 15;
   else {
     if (debug) Serial.println("Invalid message request");
     client.println(st_html + "Invalid Message" + "</html>\n");
@@ -248,65 +220,14 @@ String tx = "";
     tx += st_html + String((power*1000),0);  
     break;   
   case 5:  
-    tx += st_html + "*" + String((power*1000),0) + "*";  
+    aggtotal = 0;
+    for (int sample = 0; sample < aggmax; sample++) {
+       aggtotal += (float)aggpower[sample];   // total aggregate power samples
+    }
+    aggtotal = (float)aggtotal / aggmax; 
+    tx += st_html + String((aggtotal*1000),0);  
     break;
-  case 6: //Read Baro Temperature
-      if(bmp085_present) {
-        bt = (bmp.readTemperature()); // degrees C 
-        ftoa(bt,btmp, 1);
-        tx += st_html + "Outside Temperature " + btmp +"C";  
-      }
-      else {
-        strcpy(btmp,"not read");
-      }
-      break;
-    case 7: //Read Baro Pressure
-      if(bmp085_present) {
-        bp = bmp.readPressure() / 99.62; // 3386.00 - note MUST add decimal places
-        ftoa(bp,bprs, 1);
-        tx += st_html + "Barometric Pressure " + bprs + "hPa";  
-      }
-      else {
-        strcpy(btmp,"not read");
-      }
-      break;
-    case 8:
-      dhth = dht.readHumidity();
-      ftoa(dhth,dhhumi, 1);
-      tx += st_html + "Humidity " + dhhumi + "%";
-      break;
-    case 9:
-      dhtt = (dht.readTemperature(true)-32)/1.8; // adjusted for C not F
-      ftoa(dhtt,dhtemp, 1);
-      tx += st_html + "Inside Temperature " + dhtemp + "C";
-      state=0;
-      break;
-  
-    case 10:  
-    tx += st_html + "RC Switch 1 OFF";  
-    mySwitch.send(3572860706, 32);  // OFF
-    break; 
-  case 11:  
-    tx += st_html + "RC Switch 1 ON";  
-    mySwitch.send(3572862764, 32);    // ON
-    break;
-  case 12:  
-    tx += st_html + "RC Switch 2 OFF";  
-    mySwitch.send(3572859686, 32);  // OFF
-    break; 
-  case 13:  
-    tx += st_html + "RC Switch 2 ON";  
-    mySwitch.send(3572861738, 32);    // ON
-    break;
-  case 14:  
-    tx += st_html + "RC Switch 3 OFF";  
-    mySwitch.send(3572860193, 32);  // OFF
-    break; 
-  case 15:  
-    tx += st_html + "RC Switch 3 ON";  
-    mySwitch.send(3572862254, 32);    // ON
-    break;      
-  default :
+   default :
     tx += st_html + "Invalid Request"; 
     break;
  }
@@ -315,7 +236,7 @@ String tx = "";
 
   if (debug) {
   Serial.println("Client disconnected");
-  Serial.println(String(power,3) + " Kws");
+  Serial.println(tx);
   }
   // The client will be disconnected on function return
   // and 'client' object is destroyed
